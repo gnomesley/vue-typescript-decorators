2900f26c41f871d4ffc908fbfaf493763c281b8e
Evert van Brussel <ecvanbrussel@gmail.com>
2017-10-13 20:58:43 +0200

    improve mixin typing

--- src/functions.ts
+++ src/functions.ts
@@ -1,7 +1,12 @@
 // functional isn't decorator, but worth implementing here
-import {VClass} from './interface'
+import { VClass as Cls } from './interface'
 import Vue from 'vue'
 
-export function Mix<T extends Vue>(parent: typeof Vue, ...mixins: (typeof Vue)[]): VClass<T> {
-  return parent.extend({ mixins }) as any
+export function Mixin<A>(parent: Cls<A>): Cls<A>
+export function Mixin<A, B>(parent: Cls<A>, trait: Cls<B>): Cls<A&B>
+export function Mixin<A, B, C>(parent: Cls<A>, trait: Cls<B>, trait1: Cls<C>): Cls<A&B&C>
+export function Mixin<A, B, C, D>(parent: Cls<A>, trait: Cls<B>, trait1: Cls<C>, trait3: Cls<D>): Cls<A&B&C&D>
+export function Mixin<T>(parent: Cls<T>, ...traits: (typeof Vue)[]): Cls<T>
+export function Mixin<T>(parent: Cls<T>, ...traits: (typeof Vue)[]): Cls<T> {
+  return parent.extend({mixins: traits}) as any
 }
